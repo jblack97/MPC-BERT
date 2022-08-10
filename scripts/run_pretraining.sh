072c41da79b7566c294117a9d57e2d7b04c9cbe7
jblack97 <76516724+jblack97@users.noreply.github.com>
2022-08-10 11:30:32 +0100

    Update run_pretraining.sh

--- scripts/run_pretraining.sh
+++ scripts/run_pretraining.sh
@@ -1,6 +1,9 @@
 python run_pretraining.py --tasks NSP MLM
-python run_pretraining.py --tasks ISS
-python run_pretraining.py --tasks RUR
-python run_pretraining.py --tasks PCD
-python run_pretraining.py --tasks MSUR
-python run_pretraining.py --tasks SND
+python run_pretraining.py --tasks NSP MLM RUR
+python run_pretraining.py --tasks NSP MLM ISS
+python run_pretraining.py --tasks NSP MLM PCD
+python run_pretraining.py --tasks NSP MLM SND
+python run_pretraining.py --tasks MLM
+python run_pretraining.py --tasks MLM RUR
+
+
